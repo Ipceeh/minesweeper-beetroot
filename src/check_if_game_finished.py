e8bb8b1705b813dd062f094065a9b60e327c2744
boryskova <borysovamo@gmail.com>
2023-01-09 13:53:11 +0200

    Fixed matrix data type. Fixed matrix size calculation.

--- src/check_if_game_finished.py
+++ src/check_if_game_finished.py
@@ -2,22 +2,24 @@
 from frozendict import frozendict
 
 
-def check_if_game_finished(matrix:'list[list]', bombs:'list[tuple]', move:'frozendict[str:str,tuple]', empty_hole_char:'str') -> 'dict':
+def check_if_game_finished(matrix:list[list], bombs:list[tuple], move:frozendict[str:str,tuple]) -> dict:
     game_status = {
                     'status': 'continue',
                     'end_game_status': None,
                   }
 
-    if move['move_type'] == 'safe' and move['coordinates'] in bombs:
+    if move['type'] == 'safe' and move['coordinates'] in bombs:
         game_status['status'] = 'end_game'
         game_status['end_game_status'] = 'lose'
 
-    if move['move_type'] == 'safe' and move['coordinates'] not in bombs:
+    if move['type'] == 'safe' and move['coordinates'] not in bombs:
         open_safe_cells = 0
         for row in matrix:
-            open_safe_cells += row.count(empty_hole_char)
+            for cell in row:
+                if cell['opened']:
+                    open_safe_cells += 1
 
-        if open_safe_cells == (len(matrix) ** 2 - len(bombs)):
+        if open_safe_cells == (len(matrix) * len(matrix[0]) - len(bombs)):
             game_status['status'] = 'end_game'
             game_status['end_game_status'] = 'win'
 
