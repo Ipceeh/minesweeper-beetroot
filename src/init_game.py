13ea02b6038fa6e63b08426b314587bccdca2515
oksanastep <121014536+oksanastep@users.noreply.github.com>
2023-01-23 12:14:47 +0200

    Update init_game.py
    init-game

--- src/init_game.py
+++ src/init_game.py
@@ -1,21 +1,6 @@
-#def init_game(*args):
-    ## TODO: change char to actual ceil status
+
 import random
 from typing import Any
-
-    ## Matrix:
-    ## {
-    ##        'bomb': True / False
-    ##        'bombs_near' : 0-8
-    ##        'opened': True / False
-    ## }
-    ## Bombs:
-    ## [(1,2), (0,5) ...]
-    ## Asignee: Oksana Stepanova
-   # matrix = [[SOLID_CHAR for i in range(FIELD_SIZE)] for j in range(FIELD_SIZE)]
-    #bombs = [(random.randint(0,FIELD_SIZE-1), random.randint(0,FIELD_SIZE-1)) for i in range(BOMBS_AMOUNT)]
-    #return matrix, bombs, game_status
-
 import random
 from typing import Any
 
@@ -29,31 +14,34 @@ def init_game(field_size: int, bombs_amount: int):
             
         }
 
-    Matrix = {
+    cell = {
 
-             'bomb': True or False,
-             'bombs_near' : 0-8,
-             'opened':True or False
+             'bomb': False ,
+             'bombs_near' : 0,
+             'opened': False
 
            }
 
+    copy_cell = cell.copy()
     
-    matrix = [[0 for i in range(field_size)] for j in range(field_size)]
+    matrix = [[copy_cell for i in range(field_size)] for j in range(field_size)]
+
     bombs = []
-    neirbourhood_cells = []
-    
+        
     for i in range (bombs_amount):
 
         duplicate = True
+
         while duplicate:
-             x = random.randint(0,field_size-1)
-             y = random.randint(0,field_size-1)  
-             bomb = (y,x)
-             duplicate = bomb in bombs
+
+            x = random.randint(0,field_size-1)
+            y = random.randint(0,field_size-1)  
+            bomb = (y,x)
+            duplicate = bomb in bombs
 
         
         bombs.append(tuple((y, x)))
-        matrix[y][x] = '*'
+        matrix[y][x]['bomb'] = True
         
  
         neirbourhood_cell = [
@@ -65,37 +53,12 @@ def init_game(field_size: int, bombs_amount: int):
             ]
         
         for i,j in neirbourhood_cell:
-            if matrix[i][j] != matrix[y][x]:
-             matrix[i][j] += 1
 
-        neirbourhood_cells.append(neirbourhood_cell)
+            if not matrix[i][j]['bomb'] :
+                matrix[i][j]['bombs_near'] += 1
+        
 
-    for i in matrix:
-            print("\t ".join(str(cell) for cell in i))
-            print("")
-        # Save in declared format
-
-
-    # for row in matrix :
-    #     for cell in row:
-            
-    #         if matrix[cell][row] == bomb[y][x] in bombs:
-    #             Matrix['bomb'] = True
-    #             Matrix['bombs_near'] = 8
-    #             Matrix['opened'] = False
-    #         if matrix[cell][row] == neirbourhood_cell[y][x] in neirbourhood_cells:
-    #             Matrix['bomb'] = False
-    #             Matrix['bombs_near'] = (''.join(str(cell) for cell in i in matrix))
-    #             Matrix['opened'] = True
-    #         else:
-    #             Matrix['bomb'] = False
-    #             Matrix['bombs_near'] = 0
-    #             Matrix['opened'] = True
-
-
-
-    
-    return matrix, bombs, neirbourhood_cells, status_game, Matrix 
+    return matrix, bombs, neirbourhood_cell, status_game
 
 
 if __name__ == '__main__':
